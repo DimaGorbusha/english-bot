c3eff10fd641ee31268aba149bed47efc72b30df
DmitriyGorbunov1 <dmitrgor1@yandex.ru>
2022-05-16 22:20:20 +0500

    upd

--- bot_scripts/main.py
+++ bot_scripts/main.py
@@ -9,14 +9,3 @@ config = {
 
 bot = telebot.TeleBot(config['token'])
 
-#----Клавиатуры----
-keyboard_main_menu = types.InlineKeyboardMarkup(row_width = 1)
-main_menu_btn_signin = types.InlineKeyboardButton('🚪 Войти', callback_data = 'signin')
-main_menu_btn_signup = types.InlineKeyboardButton('🔐 Зарегистрироваться', callback_data = 'signup')
-keyboard_main_menu.add(main_menu_btn_signin, main_menu_btn_signup) 
-
-@bot.message_handler(commands=['start'])
-def start(message):	
-	bot.send_message(message.chat.id, f'👋 Привет, я бот для изучения английского языка!\n Для начала, вам надо зарегистрироваться или войти:', reply_markup = keyboard_main_menu) 
-
-bot.infinity_polling()
