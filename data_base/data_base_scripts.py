95da54fd3a60f6e534522cd175fa5c2eeb355982
DmitriyGorbunov1 <dmitrgor1@yandex.ru>
2022-05-16 14:50:29 +0500

    bd_upd

--- data_base/data_base_scripts.py
+++ data_base/data_base_scripts.py
@@ -1,21 +1,20 @@
-from hashlib import new
 import psycopg2
 
 
 class DB:
-    def DB_connect(self) -> None:
+    def __DB_connect(self) -> None: # Метод соединения с бд
         self.connection = psycopg2.connect(dbname='database', 
                                     user='postgres', 
                                     password='$NS$//1907',
                                     host='localhost')
 
 
-    def __init__(self) -> None:
-        self.DB_connect()
+    def __init__(self) -> None: # Метод инициализации объекта классаа
+        self.__DB_connect()
         self.connection.close()
 
-    def create_table(self) -> None:
-        self.DB_connect()
+    def create_table(self) -> None: # Метод создания таблиц
+        self.__DB_connect()
         try:
             with self.connection.cursor() as cursor:
                 self.connection.autocommit = True
@@ -37,17 +36,9 @@ class DB:
         finally:
             self.connection.close()
 
-    def insert_descriptin(self):
-        self.DB_connect()
-        try:
-            with self.connection.cursor() as cursor:
-                self.connection.autocommit = True
 
-        finally:
-            self.connection.close()
-
-    def insert_data_users(self, login: str, password: str, level: int, name: str, score: int) -> None: 
-        self.DB_connect()
+    def insert_data_users(self, login: str, password: str, level: int, name: str, score: int) -> None: # Метод создания новой записи в таблице пользователей
+        self.__DB_connect()
         try:
             with self.connection.cursor() as cursor:
                 self.connection.autocommit = True
@@ -59,8 +50,8 @@ class DB:
             self.connection.close()
 
     
-    def __find_login(self, login: str) -> bool:
-        self.DB_connect()
+    def __find_login(self, login: str) -> bool: # Метод поиска логина пользователя из бд
+        self.__DB_connect()
         try:
             with self.connection.cursor() as cursor:
                 self.connection.autocommit = True
@@ -73,7 +64,7 @@ class DB:
 
 
     def update_user_level(self, login: str, level: int) -> None: # Метод обновления уровня знания английского в таблице пользователей
-        self.DB_connect()
+        self.__DB_connect()
         try:
             with self.connection.cursor() as cursor:
                 self.connection.autocommit = True
@@ -84,7 +75,7 @@ class DB:
 
     
     def update_user_name(self, new_name: str, login: str) -> None: # Метод обновления пароля в таблице пользователей
-        self.DB_connect()
+        self.__DB_connect()
         try:
             with self.connection.cursor() as cursor:
                 self.connection.autocommit = True
@@ -96,7 +87,7 @@ class DB:
 
     
     def update_user_score(self, score: int, login: str) -> None: # Метод обновления пароля в таблице пользователей
-        self.DB_connect()
+        self.__DB_connect()
         try:
             with self.connection.cursor() as cursor:
                 self.connection.autocommit = True
@@ -108,7 +99,7 @@ class DB:
 
 
     def update_user_password(self, new_password: str, login: str) -> None: # Метод обновления пароля в таблице пользователей
-        self.DB_connect()
+        self.__DB_connect()
         try:
             with self.connection.cursor() as cursor:
                 self.connection.autocommit = True
@@ -116,4 +107,14 @@ class DB:
                 cursor.execute(sql_update_user_name_query, (new_password, login))
         finally:
 
+            self.connection.close()
+
+    
+    def insert_description(self):
+        self.__DB_connect()
+        try:
+            with self.connection.cursor() as cursor:
+                self.connection.autocommit = True
+
+        finally:
             self.connection.close()
